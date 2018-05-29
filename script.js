b7b4ce2326f74c18646b1944f238c4d0e072dcb5
david <david.journo.31@gmail.com>
2018-05-29 15:49:08 +0200

    adding buttons to html and minor modifications to js

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 requette.onreadystatechange = function(){
     if (requette.readyState == 4 && requette.status == 200)
     document.getElementById('reponse').innerHTML = requette.responseText;
-    requette.open('POST', 'clients.json', true)
+    requette.open('GET', 'clients.json', true)
+    requette.send()
 }; */
 
-$.getJSON({
-    url: "client.json"
-})
+$.getJSON("clients.json", function(data){
+    var items = [];
+    $.each(data )
+}
+
+$.getJSON( "ajax/test.json", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+   
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
 
