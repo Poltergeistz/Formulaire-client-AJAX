40a453c6382c423b04ebfacd88b0a46d3132bbd0
david <david.journo.31@gmail.com>
2018-05-30 15:12:32 +0200

    linting code

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-// var counter = 1;
 var clientContainer = document.getElementById('clients-info');
-var btn = document.getElementById('btn');
-btn.addEventListener("click", function () {
+
+function clickclack(i) { // Looking for user click -> i = indexOf array 
   var request = new XMLHttpRequest(); // instance 
-  request.open('GET', 'https://api.myjson.com/bins/11rupi'); // recherche le fichier json via url
-  request.onload = function () { // fonction anonnyme
-    if (request.status >= 200 && request.status < 400) {
+
+  request.open('GET', 'https://api.myjson.com/bins/11rupi'); // get url and open it
+  request.onload = function () { // anonnimous function
+     if (request.status >= 200 && request.status < 400) { // >= 200 & < 400 = OK
       console.log(request.responseText);
-      var data = JSON.parse(request.responseText); // parse le json pour récuperer uniquement l'objet de l'array
-      console.log(data[0]);
-    } else {
+      var data = JSON.parse(request.responseText); // Parse JSON file into a var called data
+      console.log(data[i]);
+     } else {
       console.log("connected but error")
     }
-    addHtml(data)
+    clientContainer.insertAdjacentHTML('beforeend', data[i].nom + "</p>" + "<p>" + data[i].prenom + "</p>" + "<p>" + data[i].âge + "</p>" + "<p>" + data[i].profession + "</p>" + "<p>" + data[i].email + "</p>" + "<p>" + data[i].téléphone + "</p>" + "<hr>")
   };
   request.send();
-});
-
-function addHtml(data) {
-  var strHtml = "";
-  for (i = 0; i < data.length; i++) {
-    strHtml += "<p>" + data[i].nom + "</p>" + "<p>" + data[i].prenom + "</p>" + "<p>" + data[i].âge + "</p>" + "<p>" + data[i].profession + "</p>" + "<p>" + data[i].email + "</p>" + "<p>" + data[i].téléphone + "</p>" + "<hr>";
-  };
-  clientContainer.insertAdjacentHTML('beforeend', strHtml);
-  // clientContainer.insertAdjacentHTML('beforeend', "testing " + "my " + "AJAX"); TEST SEQUENCE
 };
 
-
-
+//   var strHtml = "";
+//   strHtml += "<p>" + data[i].nom + "</p>" + "<p>" + data[i].prenom + "</p>" + "<p>" + data[i].âge + "</p>" + "<p>" + data[i].profession + "</p>" + "<p>" + data[i].email + "</p>" + "<p>" + data[i].téléphone + "</p>" + "<hr>";
