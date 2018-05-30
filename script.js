eae130ef683eeb98111bc02503cd75eddbda4799
david <david.journo.31@gmail.com>
2018-05-30 15:43:15 +0200

    adding a var that display all elements

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function clickclack(i) { // Looking for user click -> i = indexOf array
      if (request.status >= 200 && request.status < 400) { // >= 200 & < 400 = OK
       console.log(request.responseText);
       var data = JSON.parse(request.responseText); // Parse JSON file into a var called data
+      var clientData = "<p>" + data[i].nom + "</p>" + "<p>" + data[i].prenom + "</p>" + "<p>" + data[i].âge + "</p>" + "<p>" + data[i].profession + "</p>" + "<p>" + data[i].email + "</p>" + "<p>" + data[i].téléphone + "</p>" + "<hr>";
       console.log(data[i]);
      } else {
       console.log("connected but error")
     }
-    clientContainer.insertAdjacentHTML('beforeend', data[i].nom + "</p>" + "<p>" + data[i].prenom + "</p>" + "<p>" + data[i].âge + "</p>" + "<p>" + data[i].profession + "</p>" + "<p>" + data[i].email + "</p>" + "<p>" + data[i].téléphone + "</p>" + "<hr>")
+    clientContainer.insertAdjacentHTML('beforeend', clientData)
   };
   request.send();
 };
